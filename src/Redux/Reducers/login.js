5e1daf341a24e97be3bbb983657af70c43e83a4f
tomas <tomasdc2016@gmail.com>
2022-09-09 04:16:25 -0300

    requisito 5

--- src/Redux/Reducers/login.js
+++ src/Redux/Reducers/login.js
@@ -1,13 +1,17 @@
-import { ADD_EMAIL } from '../Actions';
+import { ADD_EMAIL, ADD_NAME } from '../Actions';
 
 const INITIAL_STATE = {
   email: '',
+  name: '',
 };
 
 function loginReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
   case ADD_EMAIL: return {
     ...state, email: action.email,
+  };
+  case ADD_NAME: return {
+    ...state, name: action.name,
   };
   default: return state;
   }
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import getHashGravatar from '../services/gravatar';
 
 class Game extends Component {
   render() {
+    const { emailReducer, nameReducer } = this.props;
     return (
-      <div>Game</div>
+      <div className="header-conteiner">
+        <h2 data-testid="header-player-name">{nameReducer}</h2>
+        <img
+          data-testid="header-profile-picture"
+          src={ getHashGravatar(emailReducer) }
+          alt="gravatar"
+        />
+        <span data-testid="header-score">Score: 0</span>
+      </div>
     );
   }
 }
 
-export default Game;
+const mapStateToProps = (state) => ({
+  emailReducer: state.loginReducer.email,
+  nameReducer: state.loginReducer.name,
+});
+
+Game.propTypes = {
+  emailReducer: PropTypes.string.isRequired,
+  nameReducer: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Game);
